99141eac832eb8151218f628e8ad5cf6bc586035
Aizhan Abenova <aizhan.zh.ab.07@gmail.com>
2022-05-14 12:21:34 -0400

    commit changes

---
+++ src/main/java/git/git1.java
@@ -0,0 +1,10 @@
+package git;
+
+public class git1 {
+    public static void main(String[] args) {
+        System.out.println("Github");
+        System.out.println("staging area");
+        System.out.println("follow me");
+        System.out.println("10/0");
+    }
+}
